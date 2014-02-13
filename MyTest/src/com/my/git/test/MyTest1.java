d94f3ebdb3e5e087e28db250c3f1e361aae56552
Tunaiyi <yangkun311@gmail.com>
2014-02-13 23:08:37 +0800

    commit 3

--- MyTest/src/com/my/git/test/MyTest1.java
+++ MyTest/src/com/my/git/test/MyTest1.java
@@ -5,6 +5,7 @@ public class MyTest1 {
 	public static void main(String[] args) {
 		System.out.println("commit 1 一次");
 		System.out.println("commit 2 两次");
+		System.out.println("commit 3 三次");
 	}
 	
 }
