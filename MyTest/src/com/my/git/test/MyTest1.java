0d811e2cb82fbf41955ac9178162548233361f74
Tunaiyi <yangkun311@gmail.com>
2014-02-18 00:31:55 +0800

    delete other local commit

--- MyTest/src/com/my/git/test/MyTest1.java
+++ MyTest/src/com/my/git/test/MyTest1.java
@@ -6,12 +6,6 @@ public class MyTest1 {
 		System.out.println("commit 1 一次");
 		System.out.println("commit 2 两次");
 		System.out.println("commit 3 三次");
-		System.out.println("other local commit 1");
-		System.out.println("other local commit 2");
-		System.out.println("other local commit 3");
-		System.out.println("other local commit 4");
-		System.out.println("other local commit 5");
-		System.out.println("other local commit 6");
 	}
 	
 }
