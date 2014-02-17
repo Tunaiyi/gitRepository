408c5e7fb1f1540b2733d1e676414279fdf1de08
Tunaiyi <yangkun311@gmail.com>
2014-02-18 00:38:36 +0800

    other local commit 3 other local commit 2 other local commit 1

--- MyTest/src/com/my/git/test/MyTest1.java
+++ MyTest/src/com/my/git/test/MyTest1.java
@@ -6,6 +6,9 @@ public class MyTest1 {
 		System.out.println("commit 1 一次");
 		System.out.println("commit 2 两次");
 		System.out.println("commit 3 三次");
+		System.out.println("other local commit 1");
+		System.out.println("other local commit 2");
+		System.out.println("other local commit 3");
 	}
 	
 }
